c12eca05ce02837047fc06d72c8079674a275051
Happiless.zhang <Happiless.zhang@fatritech.com>
2021-02-26 19:25:05 +0800

    逻辑回归

---
+++ com/happiless/02_ai_core/02_机器学习初探/04_homework_text_clustering.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# @File     : 04_homework_text_clustering.py
+# @todo     : 文本聚类
+import pandas as pd
+data = pd.read_csv('./data/abcnews-date-text.csv')
+print(data.head())
